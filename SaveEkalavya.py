81e7ea81e710d444250bccac757544e11f05decd
Shashank V Ray <shashank.v.ray@gmail.com>
2021-04-19 14:23:48 +0530

    Update SaveEkalavya.py

--- SaveEkalavya.py
+++ SaveEkalavya.py
@@ -1,3 +1,5 @@
+## 1st Place Winner Code of the competition from the YouTube Channel - Coders Colony
+## YouTube Channel - https://www.youtube.com/channel/UCUKgbFdV2XtH-YXWEk4_FWw
 def Digital(str):
 	san = ["shun","ekam","dvee","trin","catv","panc","shat","sapt","asta","nava"]
 	str+= " "
