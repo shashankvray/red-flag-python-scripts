ceb275fd1f6147472e86ce622108d5e9411a8519
Shashank V Ray <shashank.v.ray@gmail.com>
2021-05-28 15:38:10 +0530

    Add files via upload

---
+++ ShowAvailableOnMSTeams.py
@@ -0,0 +1,19 @@
+# Verbal Reading Comprehension Skills #
+
+1.  Main Idea              - 4  -->     Completed 
+2.  Simplifying            - 9  -->     Completed 
+3.  Function               - 9  -->     Completed 
+4.  Important Words        - 5  -->     Completed 
+5.  Long Passages          - 3  -->     Completed 
+6.  Inferences             - 4  -->     Completed 
+7.  Predicting             - 2  -->     Completed 
+8.  Critical Reasoning     - 3  -->     Completed 
+9.  Questions              - 6  -->     Completed 
+10. Scanning               - 2  -->     Completed 
+11. Elimination            - 3  -->     Completed 
+12. Defending              - 1  -->     Completed 
+
+            TOTAL       === 51 hours
+            Real Time   === 25 hours + 3 hours (pause/play)
+                            28 HOURS
+;
