7efdb64d1dc4336bb0ee8894a342c1908286a4f5
Shashank V Ray <shashank.v.ray@gmail.com>
2021-12-29 19:54:03 +0530

    Update exeCreator.py

--- exeCreator.py
+++ exeCreator.py
@@ -1,9 +1,8 @@
-# python setup.py build
+# python exeCreator.py build
 from cx_Freeze import setup, Executable
 
 base = None    
-
-filename = input("Enter the file name to create its .exe: (without the file extension) -   ")
+filename = input("Enter the filename(relative) or path(elsewhere) to create its .exe: (without the file extension) -   ")
 packages = list(map(str, input(f"Enter the packages used/imported in your {filename}.py file in a single line separated with space(\" \"):\n").split()))
 executables = [Executable(filename + ".py", base=base)]
 
