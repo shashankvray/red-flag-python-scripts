21d9f477e13826886971386b3a6012932ebe6f11
Shashank V Ray <shashank.v.ray@gmail.com>
2021-12-29 19:52:08 +0530

    Update exeCreator.py

--- exeCreator.py
+++ exeCreator.py
@@ -2,7 +2,7 @@
 from cx_Freeze import setup, Executable
 
 base = None    
-print("\n<<<<< MAKE SURE YOU HAVE APPENDED THE IMPORTED LIBRARIES INTO THE \"PACKAGES\" ARRAY IN THE SETUP FILE >>>>>")
+
 filename = input("Enter the file name to create its .exe: (without the file extension) -   ")
 packages = list(map(str, input(f"Enter the packages used/imported in your {filename}.py file in a single line separated with space(\" \"):\n").split()))
 executables = [Executable(filename + ".py", base=base)]
