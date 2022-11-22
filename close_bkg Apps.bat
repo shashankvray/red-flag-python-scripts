e203a3b81a672a5372a70a03a0375cc5cec5d317
Shashank V Ray <shashank.v.ray@gmail.com>
2022-11-22 16:07:46 +0530

    Update close_bkg Apps.bat

--- close_bkg Apps.bat
+++ close_bkg Apps.bat
@@ -17,59 +17,87 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 
-set /p var="Terminate Teams as well? (Enter/a_z)   : "
-echo %var%|findstr "^[A-Za-z]*$" >nulx
-if %errorlevel% == 1 (taskkill /F /IM "Teams.exe" /T)
 
+set /p var="Terminate Teams as well? ([Enter]/a_z)   : "
+echo %var%|findstr "^[A-Za-z]*$" > "%TEMP%\nulx"3
+Rem --------------------------------------------------------------------------------
+
+if %errorlevel% == 1 (
+Rem CLOSE ALL [ off work ]
+taskkill /F /IM "Teams.exe" /T
+echo "MS TEAMS HAS BEEN ASSASSINATED.... Muahahaha...."
+echo:
+echo:
+taskkill /F /IM "wscript.exe" /T) else (
+Rem OPEN TEAMS [ in work ]
+CALL "C:\Users\Shashank V Ray\AppData\Local\Microsoft\Teams\Update.exe" --processStart "Teams.exe"
+)
 
 taskkill /F /IM "FortiClient.exe" /T
+CALL "%USERPROFILE%\OneDrive - Profinch Solutions Private Limited\Java Team - Rahul Agarwal\FORTICLIENT INSTALLATION FOLDER\FortiClient\FortiTray.exe" --shutdown
 CALL "C:\Program Files\Fortinet\FortiClient\FortiTray.exe" --shutdown
-taskkill /F /IM "Spotify.exe" /T
+REM taskkill /F /IM "PowerToys.exe" /T
+REM taskkill /F /IM "PowerToys.PowerLauncher.exe" /T
+REM taskkill /F /IM "msedge.exe" /T
+REM taskkill /F /IM "msedgewebview2.exe" /T
+"C:\Users\Shashank V Ray\OneDrive - Profinch Solutions Private Limited\TMB Support\OpenVPN\bin\openvpn-gui.exe" --command disconnect openvpn-shashank.ray-inline.ovpn
+
+taskkill /F /IM "OneDrive.exe" /T
+taskkill /F /IM "SSPService.exe" /T
+taskkill /F /IM "SophosFileScanner.exe" /T
+taskkill /F /IM "TeamViewer_Service.exe" /T
+taskkill /F /IM "Appgate SDP Update Service.exe" /T
+taskkill /F /IM "TSVNCache.exe" /T
+taskkill /F /IM "Taskmgr.exe" /T
+taskkill /F /IM "XtuService.exe" /T
+taskkill /F /IM "Zoom.exe" /T
+taskkill /F /IM "CxAudioSvc.exe" /T
+taskkill /F /IM "FCDBLog.exe" /T
+taskkill /F /IM "FortiSettings.exe" /T
+taskkill /F /IM "FortiSSLVPNdaemon.exe" /T
+taskkill /F /IM "TeamViewer.exe" /T
+taskkill /F /IM "zWebview2Agent.exe" /T
+taskkill /F /IM "plsqldev.exe" /T
+taskkill /F /IM "sqldeveloper64W.exe" /T
+taskkill /F /IM "WinSCP.exe" /T
+taskkill /F /IM "TSVNCache.exe" /T
 taskkill /F /IM "ntoskrnl.exe" /T
 taskkill /F /IM "OUTLOOK.exe" /T
-taskkill /F /IM "PowerToys.exe" /T
+taskkill /F /IM "MRT.exe" /T
+taskkill /F /IM "Windows-KB890830-x64-V5.99.exe" /T
 taskkill /F /IM "Widgets.exe" /T
-taskkill /F /IM "PowerToys.FancyZones.exe" /T
-taskkill /F /IM "msedgewebview2.exe" /T
-taskkill /F /IM "PowerToys.PowerLauncher.exe" /T
 taskkill /F /IM "HxTsr.exe" /T
-taskkill /F /IM "FortiSSLVPNdaemon.exe" /T
 taskkill /F /IM "FCDBLog.exe" /T
-taskkill /F /IM "FortiTray.exe" /T
 taskkill /F /IM "SearchIndexer.exe" /T
 taskkill /F /IM "ETDCtrl.exe" /T
 taskkill /F /IM "ETDTouch.exe" /T
 taskkill /F /IM "Taskmgr.exe" /T
 taskkill /F /IM "SysInfoCap.exe" /T
-taskkill /F /IM "wscript.exe" /T
 taskkill /F /IM "vpnui.exe" /T
 taskkill /F /IM "cscript.exe" /T
 taskkill /F /IM "OfficeClickToRun.exe" /T
-taskkill /F /IM "OneDrive.exe" /T
 taskkill /F /IM "httpd.exe" /T
 taskkill /F /IM "postgres.exe" /T
 taskkill /F /IM "pg_ctl.exe" /T
 taskkill /F /IM "Cortana.exe" /T
 taskkill /F /IM "vpnagent.exe" /T
 taskkill /F /IM "Discord.exe" /T
-taskkill /F /IM "TouchpointAnalyticsClientService.exe" /T
 taskkill /F /IM "KiteService.exe" /T
 taskkill /F /IM "Kite.exe" /T
 taskkill /F /IM "Kited.exe" /T
-taskkill /F /IM "msedge.exe" /T
 taskkill /F /IM "TouchpointAnalyticsClientService.exe" /T
-taskkill /F /IM "notepad++.exe.exe" /T
 taskkill /F /IM "spoolsv.exe" /T
-taskkill /F /IM "YourPhoneServer.exe" /T
-taskkill /F /IM "YourPhone.exe" /T
 taskkill /F /IM "FortiSettings.exe" /T
 taskkill /F /IM "HxTsr.exe" /T
 taskkill /F /IM "UserOOBEBroker.exe" /T
 
-del "%USERPROFILE%\OneDrive - Profinch Solutions Private Limited\Desktop\nulx" /s /f /q
+REM del "%USERPROFILE%\OneDrive - Profinch Solutions Private Limited\Desktop\nulx" /s /f /q
+del "%USERPROFILE%\AppData\Local\pip\cache" /s /f /q
 rmdir "%TEMP%\" /q /s
+rmdir "%TEMP%\*" /q /s
+
+del "%USERPROFILE%\OneDrive - Profinch Solutions Private Limited\Pictures\Screenshots*" /s /f /q
+del "%TEMP%\*.*" /s /f /q
 @taskkill /f /im explorer.exe >nul
 @start explorer.exe >nul
-del "%TEMP%\*.*" /s /f /q
-
 REM pause
